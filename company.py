95a5f5f7949175883eb192dd22d604e0bd7a8922
sohale <sohale@gmail.com>
2017-01-18 23:11:20 +0000

    lint fix

--- company.py
+++ company.py
@@ -1,30 +1,33 @@
+""" Company """
 class CompanyEntry(object):
 
-   class CT():
-      """ Company types """
-      COMMON = 2345
-      PREFERRED = 6789
+    class CT():
+        """ Company types """
+        COMMON = 2345
+        PREFERRED = 6789
 
-   def __init__(self, abbrev, company_type, last_dividend, fixed_dividend, par_value):
-      self.ct = company_type
-      assert self.check()
+    def __init__(self, abbrev, company_type, last_dividend, fixed_dividend, par_value):
+        self.ct = company_type
+        assert self.check()
 
-   def check(self):
-      """ Class invariant. Asserts consistency of data in this object. """
-      if self.ct == CompanyEntry.CT.COMMON:
-          pass
-      elif self.ct == CompanyEntry.CT.PREFERRED:
-          pass
-      else:
-          raise Exception("Company type can only be either CT.COMMON or CTPREFERRED ")
+    def check(self):
+        """ Class invariant. Asserts consistency of data in this object. """
+        if self.ct == CompanyEntry.CT.COMMON:
+            pass
+        elif self.ct == CompanyEntry.CT.PREFERRED:
+            pass
+        else:
+            raise Exception("Company type can only be either CT.COMMON or CTPREFERRED ")
 
-      #todo: more
+        #todo: more
 
-      return True  # enable `assert` usage
-
-   def calculate_dividend_yield():
-      if self.ct == CompanyEntry.CT.COMMON:
-          pass
-      elif self.ct == CompanyEntry.CT.PREFERRED:
-          pass
+        return True  # enable `assert` usage
+        
+    def calculate_dividend_yield():
+        """ calculate the Divident Yield based on the given market price.
+        This is one of the endpoints."""
+        if self.ct == CompanyEntry.CT.COMMON:
+            pass
+        elif self.ct == CompanyEntry.CT.PREFERRED:
+            pass
 
