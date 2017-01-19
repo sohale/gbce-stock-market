f216e221e901be02c8dafa5d394688a16eb5519d
sohale <sohale@gmail.com>
2017-01-19 02:22:18 +0000

    more linting: indentation

--- p1.py
+++ p1.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+""" GBCS Stock Demo """
+
 import math
 import numpy as np
 import datetime
-
-import datetime
-import numpy as np
 
 from trade import Trade
 
 
 
 def example_100trades():
+    """ Gnerates 100 random trades to experiment with."""
     all_trades = []
 
 
     for i in range(100):
-        t = Trade(timestamp=np.datetime64(datetime.datetime.now(), 'ms'), quantity=13, buysell_type=Trade.BUY, trade_price=1.00);
+        t = Trade(timestamp=np.datetime64(datetime.datetime.now(), 'ms'), \
+            quantity=3+(i % 5), buysell_type=Trade.BUY, trade_price=1.00)
         t.invar()
         all_trades.append(t)
     print repr(all_trades)
@@ -28,9 +29,10 @@ def example_100trades():
     assert a1rec.shape == (100,)
 
 def get_recent_trades(time_diff_ms): #(from_ms, to_ms=0):
+    """ Makes a collection of Trades in the given interval."""
     now_ms = np.datetime64(datetime.datetime.utcnow(), 'ms')
     from_ms = now_ms - time_diff_ms
-    raise Exception("Not implemented")
+    raise Exception("Not implemented "+str(from_ms)+" "+str(now_ms))
 
 def demo_get15min():
     # Get trades in past 15 minute
@@ -38,7 +40,7 @@ def demo_get15min():
     MIN = SEC * 60
     #from_ms = now_ms - 15*MIN
     ts = get_recent_trades(15*MIN)
-    print ts
+    print ">>ts<<", ts
 
 # main: test
 if __name__ == "__main__":
@@ -50,24 +52,24 @@ if __name__ == "__main__":
 # ===================================================================================
 
 def checkmoney(x):
-   # actual absolute money, not rate (e.g. price per share)
-   # Makes sure the amount is in cents, not less
-   assert(abs(int(x*100)- (x*100)) < 0.000001)
+    # actual absolute money, not rate (e.g. price per share)
+    # Makes sure the amount is in cents, not less
+    assert abs(int(x*100)- (x*100)) < 0.000001
 
 def fixmoney_floor(x):
-   result = math.floor(x*100) * 0.0100000000
-   checkmoney(result)
-   return result
+    result = math.floor(x*100) * 0.0100000000
+    checkmoney(result)
+    return result
 
 def fixmoney_ceil(x):
-   result = math.ceil(x*100) * 0.0100000000
-   checkmoney(result)
-   return result
+    result = math.ceil(x*100) * 0.0100000000
+    checkmoney(result)
+    return result
 
 def fixmoney_round(x):
-   result = math.round(x*100) * 0.0100000000
-   checkmoney(result)
-   return result
+    result = math.round(x*100) * 0.0100000000
+    checkmoney(result)
+    return result
 
 
 market_price = 1.00
@@ -96,6 +98,4 @@ def calculate_Volume_Weighted_Stock_Price(self):
 def calculate_GBCE(self):
     return -1
 
-# dividend_yield = 
-
-
+# dividend_yield =
